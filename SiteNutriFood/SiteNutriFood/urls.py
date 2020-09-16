2bc793f37c4dc99b4b89b3fd7540f0f9ea1d21d6
PierreRWCS <61827591+PierreRWCS@users.noreply.github.com>
2020-09-16 16:20:20 +0200

    version debuger

--- SiteNutriFood/SiteNutriFood/urls.py
+++ SiteNutriFood/SiteNutriFood/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#]
+from . import views
+
 urlpatterns = [
+    #path('SiteNutriFood/', include('SiteNutriFood.urls')),
     path('admin/', admin.site.urls),
+    #path('', views.index, name='index'),
 ]
